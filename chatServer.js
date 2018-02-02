6e38f560e90152e9f15d8f91040ec0b71d7402a1
emtseng <etseng42@gmail.com>
2018-02-02 18:54:42 -0500

    updates chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -16,7 +16,7 @@ var serverPort = 8000;
 app.use(express.static('public')); // find pages in public directory
 
 // start the server and say what port it is on
-http.listen(serverPort, function() {
+http.listen(serverPort, function () {
   console.log('listening on *:%s', serverPort);
 });
 //----------------------------------------------------------------------------//
@@ -25,91 +25,76 @@ http.listen(serverPort, function() {
 //---------------------- WEBSOCKET COMMUNICATION -----------------------------//
 // this is the websocket event handler and say if someone connects
 // as long as someone is connected, listen for messages
-io.on('connect', function(socket) {
+io.on('connect', function (socket) {
   console.log('a new user connected');
   var questionNum = 0; // keep count of question, used for IF condition.
-  socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
+  socket.on('loaded', function () {// we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-  setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
+    socket.emit('answer', "Helloooo! I am \"___*-\", a simple chatbot example."); //We start with the introduction;
+    setTimeout(timedQuestion, 2500, socket, "What's your name?"); // Wait a moment and respond with a question.
 
-});
-  socket.on('message', (data)=>{ // If we get a new message from the client we process it;
-        console.log(data);
-        questionNum= bot(data,socket,questionNum);	// run the bot function with the new message
-      });
-  socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
-    console.log('user disconnected');
+  });
+  socket.on('message', (data) => { // If we get a new message from the client we process it;
+    console.log('Received data: ', data);
+    questionNum = bot(data, socket, questionNum);	// run the bot function with the new message
+  });
+  socket.on('disconnect', function () { // This function  gets called when the browser window gets closed
+    console.log('User disconnected :(');
   });
 });
 
 //--------------------------CHAT BOT FUNCTION-------------------------------//
-function bot(data,socket,questionNum) {
-  var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
+function bot(data, socket, questionNum) {
+  var input = data; // This is generally really terrible from a security point of view. ToDo: avoid code injection
   var answer;
   var question;
   var waitTime;
 
-/// These are the main statments that make up the conversation.
-  if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
-  waitTime =2000;
-  question = 'How old are you?';			    	// load next question
-  }
-  else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
-  waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
-  }
-  else if (questionNum == 2) {
-  answer= ' Cool! I have never been to ' + input+'.';
-  waitTime =2000;
-  question = 'Whats your favorite Color?';			    	// load next question
-  }
-  else if (questionNum == 3) {
-  answer= 'Ok, ' + input+' it is.';
-  socket.emit('changeBG',input.toLowerCase());
-  waitTime = 2000;
-  question = 'Can you still read the font?';			    	// load next question
-  }
-  else if (questionNum == 4) {
-    if(input.toLowerCase()==='yes'|| input===1){
-      answer = 'Perfect!';
-      waitTime =2000;
-      question = 'Whats your favorite place?';
-    }
-    else if(input.toLowerCase().contains('no') || input===0){
-        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said before.
-        answer='How about now?'
-        question='';
-        waitTime = 0;
-        questionNum--; // Here we go back in the question number. This can end up in a loop.
-    }else{
-      answer=' I did not understand you. Can you please answer with simply with yes or no.'
-      question='';
-      questionNum--;
-      waitTime =0;
-    }
-  // load next question
-  }
-  else{
-    answer = 'I have nothing more to say!';// output response
-    waitTime = 0;
-    question = '';
+  switch (questionNum) {
+    case 0:
+      answer = 'Hello ' + input + ' :-)';// output response
+      waitTime = 2000;
+      question = 'How old are you?';			    	// load next question
+      break
+    case 1:
+      answer = 'Really, ' + input + ' years old? So that means you where born in ' + (2018 - parseInt(input));// output response
+      waitTime = 5000;
+      question = 'What\'s your mother\'s maiden name?';			    	// load next question
+      break
+    case 2:
+      answer = 'That\'s cool. I\'m a robot, so I don\'t think I have a mom. But yours sounds nice.';
+      waitTime = 5000;
+      question = 'How about your first pet? What were they called?';			    	// load next question
+      break
+    case 3:
+      answer = `I always wanted a pet. But I'm a robot, so. ${input} must have been a great pet.`;
+      // socket.emit('changeBG', input.toLowerCase());
+      waitTime = 5000;
+      question = `I'm so curious about your life now. What was the name of the street that you grew up on?`;			    	// load next question
+      break
+    case 4:
+      answer = `${input} sounds so idyllic. I grew up on a factory floor.`
+      waitTime = 5000
+      question = ''
+      break
+    default:
+      answer = 'I have nothing to say!';// output response
+      waitTime = 0;
+      question = '';
+      break
   }
 
-
-/// We take the changed data and distribute it across the required objects.
-  socket.emit('answer',answer);
-  setTimeout(timedQuestion, waitTime,socket,question);
-  return (questionNum+1);
+  /// We take the changed data and distribute it across the required objects.
+  socket.emit('answer', answer);
+  setTimeout(timedQuestion, waitTime, socket, question);
+  return (questionNum + 1);
 }
 
-function timedQuestion(socket,question) {
-  if(question!=''){
-  socket.emit('question',question);
-}
-  else{
+function timedQuestion(socket, question) {
+  if (question !== '') {
+    socket.emit('question', question);
+  }
+  else {
     //console.log('No Question send!');
   }
 
