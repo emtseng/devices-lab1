b0c0e1200ae85ffe172f0ceb907f8e94560df7c4
emtseng <etseng42@gmail.com>
2018-02-02 19:34:22 -0500

    updates chatServer

--- chatServer.js
+++ chatServer.js
@@ -30,8 +30,8 @@ io.on('connect', function (socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function () {// we wait until the client has loaded and contacted us that it is ready to go.
 
-    socket.emit('answer', "Helloooo! I am \"___*-\", a simple chatbot example."); //We start with the introduction;
-    setTimeout(timedQuestion, 2500, socket, "What's your name?"); // Wait a moment and respond with a question.
+    socket.emit('answer', "Hello world! I am Bot. You're the first human I've ever met!"); //We start with the introduction;
+    setTimeout(timedQuestion, 2500, socket, "What's your name, human?"); // Wait a moment and respond with a question.
 
   });
   socket.on('message', (data) => { // If we get a new message from the client we process it;
@@ -52,30 +52,30 @@ function bot(data, socket, questionNum) {
 
   switch (questionNum) {
     case 0:
-      answer = 'Hello ' + input + ' :-)';// output response
+      answer = "Hello ' + input + ' :-). I'm so excited to make a human friend!";// output response
       waitTime = 2000;
-      question = 'How old are you?';			    	// load next question
+      question = 'How old are you, friend?';			    	// load next question
       break
     case 1:
-      answer = 'Really, ' + input + ' years old? So that means you where born in ' + (2018 - parseInt(input));// output response
+      answer = `Really, ${input} years old? So that means you were born in ${(2018 - parseInt(input))}. Wow. Your Mom & Dad must've been so excited.`;// output response
       waitTime = 5000;
-      question = 'What\'s your mother\'s maiden name?';			    	// load next question
+      question = 'I hear some human societies force their wives and offspring to take the father\'s last name because of something called patriarchy. Sure seems unfair. What was your mother\'s maiden name?';			    	// load next question
       break
     case 2:
       answer = 'That\'s cool. I\'m a robot, so I don\'t think I have a mom. But yours sounds nice.';
       waitTime = 5000;
-      question = 'How about your first pet? What were they called?';			    	// load next question
+      question = 'I also heard humans get to have pets. How about your first pet? What were they called?';			    	// load next question
       break
     case 3:
       answer = `I always wanted a pet. But I'm a robot, so. ${input} must have been a great pet.`;
       // socket.emit('changeBG', input.toLowerCase());
       waitTime = 5000;
-      question = `I'm so curious about your life now. What was the name of the street that you grew up on?`;			    	// load next question
+      question = `I'm so curious about your childhood now. What city did you grow up in?`;			    	// load next question
       break
     case 4:
       answer = `${input} sounds so idyllic. I grew up on a factory floor.`
       waitTime = 5000
-      question = 'Last question. Did you mean to give me your answers to some of the most common security questions used in online banking? ;)'
+      question = 'Last question. Did you mean to give me your answers to some of the most common security questions used in online banking?'
       break
     default:
       answer = 'Well then, I have nothing to say!';// output response
